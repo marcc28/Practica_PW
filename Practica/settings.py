725b6a12ebda68e6ff3a9ad476dc1ab090647d1a
marcc <14marc04@gmail.com>
2025-04-07 12:12:51 +0200

    Refactor project + merge admin

--- Practica/settings.py
+++ Practica/settings.py
@@ -53,7 +53,7 @@ ROOT_URLCONF = 'Practica.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
+        'DIRS': [BASE_DIR / 'web/templates']
         ,
         'APP_DIRS': True,
         'OPTIONS': {
