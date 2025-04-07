3d20d0c8b12a9b109c25ab51799fcf166070196b
marcc <14marc04@gmail.com>
2025-04-07 12:58:39 +0200

    refactor urls.py

--- web/urls.py
+++ web/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
-from django.contrib import admin
+
 from . import views
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', views.logout_view, name='logout'),
     path('register/', views.register, name='register'),
