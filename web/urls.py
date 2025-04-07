e7415ef782b7dcda1c64ea62453dd5a470d2944e
marcc <14marc04@gmail.com>
2025-04-07 12:49:14 +0200

    urls.py implemented

--- web/urls.py
+++ web/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
+from django.contrib import admin
 from . import views
 
+
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('admin/', admin.site.urls),
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', views.logout_view, name='logout'),
     path('register/', views.register, name='register'),
-    path('books/', views.books, name='books'),
 ] 
