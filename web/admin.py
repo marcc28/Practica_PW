23a848917ee0320187539ac5e0239d96f1879c60
marcc <14marc04@gmail.com>
2025-04-07 19:40:05 +0200

    refactor admin.py

--- web/admin.py
+++ web/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
-from .models import Match, Season, Team, Player
+from .models import Match, Team, Player
 
 admin.site.register(Match)
-admin.site.register(Season)
 admin.site.register(Team)
 admin.site.register(Player)
