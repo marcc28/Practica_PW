81dcb9a4e85984464043c000d100d6fc1447b98b
marcc <14marc04@gmail.com>
2025-04-28 10:29:06 +0200

    urls.py added the urls for creating, editing and deleting teams

--- web/urls.py
+++ web/urls.py
@@ -1,11 +1,15 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
 from . import views
-
+from .views import TeamCreateView, TeamUpdateView, TeamDeleteView
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', views.logout_view, name='logout'),
     path('register/', views.register, name='register'),
+    path('teams/create/', TeamCreateView.as_view(), name='team-create'),
+    path('teams/<int:pk>/edit', TeamUpdateView.as_view(), name='team-edit'),
+    path('teams/<int:pk>/delete/', TeamDeleteView.as_view(), name='team-delete'),
+
 ] 
