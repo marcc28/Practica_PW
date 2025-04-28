aa1138477bcda5b4e6021560db5b63aa05d241a2
marcc <14marc04@gmail.com>
2025-04-28 10:18:01 +0200

    views.py added classes to create, update and delete a team

--- web/views.py
+++ web/views.py
@@ -1,8 +1,11 @@
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.shortcuts import render, redirect
 from django.contrib.auth import logout as auth_logout
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib import messages
-from django.contrib.auth.decorators import login_required
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, UpdateView, DeleteView
+from .models import Team
+from .forms import TeamForm
 # from .models import Book
 
 # Create your views here.
@@ -23,3 +26,32 @@ def register(request):
     else:
         form = UserCreationForm()
     return render(request, 'register.html', {'form': form})
+
+class TeamCreateView(LoginRequiredMixin, CreateView):
+    model = Team
+    form_class = TeamForm
+    template_name = 'team_create.html'
+    success_url = reverse_lazy('teams')
+
+    def form_valid(self, form):
+        form.instance.creador = self.request.user
+        return super().form_valid(form)
+
+class TeamUpdateView(LoginRequiredMixin, UpdateView):
+    model = Team
+    form_class = TeamForm
+    template_name = 'team_create.html'
+    success_url = reverse_lazy('teams')
+
+    def test_func(self):
+        equip = self.get_object()
+        return self.request.user == equip.creador
+
+class TeamDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Team
+    template_name = 'team_confirm_delete.html'
+    success_url = reverse_lazy('teams')
+
+    def test_func(self):
+        equip = self.get_object()
+        return self.request.user == equip.creador
