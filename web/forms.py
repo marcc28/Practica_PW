1e8b02a4c82cf646fa4b611ca3afd54c71db1e81
marcc <14marc04@gmail.com>
2025-05-14 10:21:05 +0200

    added to forms.py player and team

--- web/forms.py
+++ web/forms.py
@@ -1,8 +1,18 @@
 from django import forms
-from .models import Match
+from .models import Match, Team, Player
 
 
 class MatchForm(forms.ModelForm):
     class Meta:
         model = Match
-        fields = ['home_team', 'away_team', 'utc_date']
+        fields = ['home_team', 'away_team', 'utc_date', 'home_score', 'away_score', 'winner']
+
+class TeamForm(forms.ModelForm):
+    class Meta:
+        model = Team
+        fields = ['name', 'tla', 'address', 'players']
+
+class PlayerForm(forms.ModelForm):
+    class Meta:
+        model = Player
+        fields = ['name','nationality', 'position', 'shirt_number', 'current_team']
