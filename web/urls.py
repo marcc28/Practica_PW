3d6f86b821378862b2889f89c00d59a336976add
marcc <14marc04@gmail.com>
2025-05-14 10:30:08 +0200

    modified urls.py added player and team creation, edit, and delete

--- web/urls.py
+++ web/urls.py
@@ -11,5 +11,11 @@ urlpatterns = [
     path('match/create/', MatchCreateView.as_view(), name='match-create'),
     path('match/<int:pk>/edit', MatchUpdateView.as_view(), name='match-edit'),
     path('match/<int:pk>/delete/', MatchDeleteView.as_view(), name='match-delete'),
+    path('team/create/', TeamCreateView.as_view(), name='team-create'),
+    path('team/<int:pk>/edit', TeamUpdateView.as_view(), name='team-edit'),
+    path('team/<int:pk>/delete/', TeamDeleteView.as_view(), name='team-delete'),
+    path('player/create/', PlayerCreateView.as_view(), name='player-create'),
+    path('player/<int:pk>/edit', PlayerUpdateView.as_view(), name='player-edit'),
+    path('player/<int:pk>/delete/', PlayerDeleteView.as_view(), name='player-delete'),
 
-] 
+]
